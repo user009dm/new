c9097651815ba19c873de41f067d2b0df4494ec6
user009dm <161681994+user009dm@users.noreply.github.com>
2024-07-22 12:02:37 +0500

    Update b.ps1

--- b.ps1
+++ b.ps1
@@ -1,4 +1,3 @@
 Invoke-WebRequest -Uri 'https://www.krutube.pro/download/krutubel/v10.2.0.0/krutube_10.2.0.0.zip' -OutFile "$env:USERPROFILE\Desktop\krutube_10.2.0.0.zip"
 Expand-Archive -Path "$env:USERPROFILE\Desktop\krutube_10.2.0.0.zip" -DestinationPath "$env:USERPROFILE\Desktop"
 Start-Process -FilePath "$env:USERPROFILE\Desktop\KruTube\KruTube.exe" -WindowStyle Maximized
-Exit # This will keep the window open so you can see any errors
