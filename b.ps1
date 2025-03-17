afb48a6b2e9b856c2c598659a913bbfdafed3c71
user009dm <161681994+user009dm@users.noreply.github.com>
2025-03-17 10:30:20 +0500

    Update b.ps1

--- b.ps1
+++ b.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri 'https://www.krutube.pro/download/krutubel/v11.1.0.0/krutube_11.1.0.0.zip' -OutFile "$env:USERPROFILE\Desktop\krutube_10.3.0.0.zip"
+Invoke-WebRequest -Uri 'https://www.krutube.pro/download/krutubel/v11.3.0.0/krutube_11.3.0.0.zip' -OutFile "$env:USERPROFILE\Desktop\krutube_10.3.0.0.zip"
 Expand-Archive -Path "$env:USERPROFILE\Desktop\krutube_10.3.0.0.zip" -DestinationPath "$env:USERPROFILE\Desktop"
 Start-Process -FilePath "$env:USERPROFILE\Desktop\KruTube\KruTube.exe" -WindowStyle Maximized
 Exit
